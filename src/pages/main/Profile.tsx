2a2032338e861145d9bdc51b532080c270cf8b6c
Sebastián <schaquila@autonoma.edu.pe>
2023-07-02 13:42:08 -0500

    profile page

--- src/pages/main/Profile.tsx
+++ src/pages/main/Profile.tsx
@@ -1,11 +1,34 @@
 import { Header } from "../../components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 import styles from "../../styles/Profile.module.css";
 
 function Profile() {
+  const navigate = useNavigate();
   const [selectedButton, setSelectedButton] = useState<
     "personal" | "actualizacion"
   >("personal");
+  const [, setLogout] = useState<boolean>(false);
+  const [userData, setUserData] = useState<any>(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+    setLogout(true);
+  };
+
+  const getUserData = () => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setUserData(JSON.parse(user));
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div className={styles.background}>
       <Header showHero={false} />
@@ -40,64 +63,72 @@ function Profile() {
             </button>
           </div>
         </div>
-        <div className={styles.profile_info}>
-          <p className={styles.tittle_info}>Datos Personales</p>
+        <div className={styles.profile_info_container}>
+          <p className={styles.profile_info_tittle}>Datos Personales</p>
+          {}
           <div className={styles.detail_info}>
             <div className={styles.group_info}>
-              <div className={styles.fullname_info}>
+              <div className={styles.profile_info}>
                 <p className={styles.label_info}>Nombre Completo</p>
-                <input type="text" placeholder="Jhon Doe" />
+                <input
+                  type="text"
+                  readOnly
+                  value={userData?.fullname}
+                  className={styles.input_info}
+                  placeholder="Jhon Doe"
+                />
               </div>
 
-              <div className={styles.fullname_info}>
+              <div className={styles.profile_info}>
                 <p className={styles.label_info}>Correo electrónico</p>
-                <input type="text" placeholder="example@example.com" />
+                <input
+                  type="text"
+                  readOnly
+                  value={userData?.email}
+                  className={styles.input_info}
+                  placeholder="example@example.com"
+                />
               </div>
             </div>
             <div className={styles.group_info}>
-              <div className={styles.fullname_info}>
+              <div className={styles.profile_info}>
                 <p className={styles.label_info}>Teléfono</p>
-                <input type="text" placeholder="123456789" />
+                <input
+                  type="text"
+                  readOnly
+                  value={userData?.phone}
+                  className={styles.input_info}
+                  placeholder="123456789"
+                />
               </div>
-
-              <div className={styles.fullname_info}>
+              {/*}
+              <div className={styles.profile_info}>
                 <p className={styles.label_info}>Contraseña</p>
-                <input type="text" placeholder="****************" />
+                <input
+                  type="text"
+                  readOnly
+                  value={userData?.password}
+                  className={styles.input_info}
+                  placeholder="****************"
+                />
+              </div>
+            {*/}
+              <div className={styles.profile_info}>
+                <p className={styles.label_info}>DNI</p>
+                <input
+                  type="text"
+                  readOnly
+                  value={userData?.dni}
+                  className={styles.input_info}
+                />
               </div>
             </div>
           </div>
-          <button className={styles.btn_add}>
-            <p>Actualizar Datos</p>
-          </button>
-        </div>
-        <div className={styles.profile_info}>
-          <p className={styles.tittle_info}>Datos Personales</p>
-          <div className={styles.detail_info}>
-            <div className={styles.group_info}>
-              <div className={styles.fullname_info}>
-                <p className={styles.label_info}>Nombre Completo</p>
-                <input type="text" placeholder="Jhon Doe" />
-              </div>
-
-              <div className={styles.fullname_info}>
-                <p className={styles.label_info}>Correo electrónico</p>
-                <input type="text" placeholder="example@example.com" />
-              </div>
-            </div>
-            <div className={styles.group_info}>
-              <div className={styles.fullname_info}>
-                <p className={styles.label_info}>Teléfono</p>
-                <input type="text" placeholder="123456789" />
-              </div>
-
-              <div className={styles.fullname_info}>
-                <p className={styles.label_info}>Contraseña</p>
-                <input type="text" placeholder="****************" />
-              </div>
-            </div>
-          </div>
-          <button className={styles.btn_add}>
-            <p>Actualizar Datos</p>
+          <button className={styles.logout_btn}>
+            <BiLogOut />
+            <p className={styles.logout_text} onClick={handleLogout}>
+              Cerrar sesión
+            </p>
           </button>
         </div>
       </div>
